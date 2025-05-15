dbd1a5dabc5c6b1a56c7868afc2432e6e56bd169
Charles Sprayberry <me@cspray.io>
2025-05-15 10:58:19 -0400

    Finalize builder deprecation ADR

--- src/DeprecateDefinitionBuilderImplementations.php
+++ src/DeprecateDefinitionBuilderImplementations.php
@@ -2,6 +2,7 @@
 
 namespace Cspray\AnnotatedContainer\ArchitecturalDecisionRecords;
 
+use Attribute;
 use Cspray\ArchitecturalDecision\DecisionStatus;
 use Cspray\ArchitecturalDecision\DocBlockArchitecturalDecision;
 use DateTimeImmutable;
@@ -12,10 +13,22 @@ use DOMElement;
  *
  * ## Context
  *
- *
+ * Starting with the initial versions of Annotated Container most Definitions that make up a ContainerDefinition are
+ * created through a series of "builder" objects using a fluent API. Ultimately, even the functional API utilizes the
+ * builder API when you aren't using attributes to declare your ContainerDefinition. The intent of these builder objects
+ * is to provide a singular, correct method for creating implementations for the interfaces that make up a
+ * ContainerDefinition. In practice, these objects proved to be more difficult to use than expected, and led to an
+ * increase in maintenance requirements.
  *
  * ## Decision
+ *
+ * The fluent API for creating definitions proved to be clunky to use, hard to test properly, and created lots of
+ * static analysis issues. In v3, these issues started to exacerbate when we needed to make changes to the underlying
+ * Definition interfaces and their corresponding builders. Instead of continuing with this subpar system, it was
+ * removed in v3 in favor of a concrete Factory class that ensures resultant Definitions are valid and proper without
+ * having to rely on a chain of fluent API calls.
  */
+#[Attribute(Attribute::TARGET_CLASS)]
 final class DeprecateDefinitionBuilderImplementations extends DocBlockArchitecturalDecision {
 
     public function date() : DateTimeImmutable {
